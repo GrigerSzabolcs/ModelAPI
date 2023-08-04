5605177894dcc95f6c32844000d85002469701d4
GrigerSzabolcs <43068223+GrigerSzabolcs@users.noreply.github.com>
2023-08-04 13:53:43 +0200

    Add files via upload

--- server.py
+++ server.py
@@ -6,15 +6,25 @@ from flask import Flask, request, jsonify
 from keras_preprocessing.text import tokenizer_from_json
 from transformers import BertTokenizer, TFBertModel
 from tqdm import tqdm
-
-with open("tokenizer.json") as f:
-    data = json.load(f)
-    tokenizer = tokenizer_from_json(data)
+from azure.storage.blob import BlobServiceClient
 
 app = Flask(__name__)
 
+connection_string = "DefaultEndpointsProtocol=https;AccountName=modelstoragegsz;AccountKey=7IvuzeIAeggwekbAXIZ2p+LL38L5wskd8a92YQSl8wja+cE5PiusvxUGZoT7SrD/vFGGvTfqsIqt+AStGC6b9g==;EndpointSuffix=core.windows.net"
+container_name = "model"
+blob_name = "BERT_model.h5"
+file_name = "MODEL.h5"
+
+blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+blob_client = blob_service_client.get_blob_client(container_name, blob_name)
+
+with open(file_name, "wb") as download_file:
+    download_file.write(blob_client.download_blob().readall())
+
+
+print("Blob accessed")
 bert_tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
-model = keras.models.load_model("BERT_model.h5", custom_objects={"TFBertModel": TFBertModel})
+model = keras.models.load_model("MODEL.h5", custom_objects={"TFBertModel": TFBertModel})
 
 @app.route('/api',methods=['POST'])
 def predict():
