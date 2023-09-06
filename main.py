2cdceab1ca9d771882e80a9964bc63b078d33acc
GrigerSzabolcs <43068223+GrigerSzabolcs@users.noreply.github.com>
2023-09-06 18:15:34 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -29,45 +29,6 @@ model = keras.models.load_model("MODEL.h5", custom_objects={"TFBertModel": TFBer
 
 @app.route('/api',methods=['POST'])
 def predict():
-    #data = request.get_json(force=True)
-
-    #input_ids = []
-    #attention_masks = []
-    #for sent in tqdm(data['text']):
-    #    bert_inp = bert_tokenizer.encode_plus(sent, add_special_tokens=True, max_length=128, pad_to_max_length=True,
-    #                                          return_attention_mask=True)
-    #    input_ids.append(bert_inp['input_ids'])
-    #    attention_masks.append(bert_inp['attention_mask'])
-
-    #input_ids = np.asarray(input_ids)
-    #attention_masks = np.array(attention_masks)
-
-    #prediction = model.predict([input_ids, attention_masks])
-
-    #answers = []
-    #for x in prediction:
-    #    if x[0] > x[1] and x[0] > x[2]:
-    #        answers.append("negative")
-    #    elif x[1] > x[0] and x[1] > x[2]:
-    #        answers.append("neutral")
-    #    else:
-    #        answers.append("positive")
-    answers = "bertendpoint"
-    return jsonify(answers)
-"""
-blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-blob_client = blob_service_client.get_blob_client(container_name, blob_name)
-
-with open(file_name, "wb") as download_file:
-    download_file.write(blob_client.download_blob().readall())
-
-
-print("Blob accessed")
-bert_tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
-model = keras.models.load_model("MODEL.h5", custom_objects={"TFBertModel": TFBertModel})
-
-@app.route('/api',methods=['POST'])
-def predict():
     data = request.get_json(force=True)
 
     input_ids = []
@@ -92,9 +53,6 @@ def predict():
         else:
             answers.append("positive")
     return jsonify(answers)
-
-
-"""
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
